4f424359530ee0501340755c04787d699032c94a
okome00 <m.okome33@gmail.com>
2022-04-03 06:17:07 +0000

    [Update]管理者：商品機能実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,12 +9,15 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
     @genres = Genre.all
   end
-  
+
   # 商品新規登録アクション
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_item_path(item.id)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render :new
+    end
   end
 
   # 商品詳細画面アクション
@@ -25,22 +28,24 @@ class Admin::ItemsController < ApplicationController
   # 商品編集画面アクション
   def edit
     @item = Item.find(params[:id])
+    @genres = Genre.all
   end
-  
+
   # 商品編集アクション
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to admin_item_path(item.id)
+      redirect_to admin_item_path(@item)
     else
       render :edit
     end
   end
-  
+
   # 新規登録データのストロングパラメータ
   private
+
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
-  
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,18 @@ class Item < ApplicationRecord
   # ActiveStoreage：画像使用宣言
   has_one_attached :image
 
+  # 画像が登録されていない場合のエラー回避
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  # 消費税を加えた商品価格
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+
 end
