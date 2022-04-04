54ed1fe1a07d7b5007de99d17b01da16e849af74
okome00 <m.okome33@gmail.com>
2022-04-04 14:36:00 +0000

    [Update]管理者：レイアウト調整中

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+  #Sign_in後の遷移先
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
+
+  # Sign_out後の遷移先
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
